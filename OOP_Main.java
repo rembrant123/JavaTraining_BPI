ea7b494231ca79b57107cde107104664422b952b
Rembrant B. Tortogo <rbtortogo@headoffice.corp.bpi.com.ph>
2025-12-04 16:09:03 +0800

    oop act2

---
+++ OOP_Main.java
@@ -0,0 +1,14 @@
+package OOP_Day4;
+
+public class OOP_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WhiteBoard wb = new WhiteBoard();
+		
+		int sum= wb.addNumbers(5,3);
+		System.out.println("The total is: "+String.valueOf(sum));
+
+	}
+
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,10 @@
+package OOP_Day4;
+
+public class WhiteBoard {
+	
+	int addNumbers(int addend1, int addend2) {
+		int total = addend1 + addend2;
+		return total;
+	}
+
+}
