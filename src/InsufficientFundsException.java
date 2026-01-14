d7858df365814605fb36ba6a0d1d650a89720cd1
Rembrant B. Tortogo <rbtortogo@headoffice.corp.bpi.com.ph>
2026-01-14 14:41:29 +0800

    M4 Activity 7

---
+++ src/InsufficientFundsException.java
@@ -0,0 +1,40 @@
+package m4_activity7;
+
+public class InsufficientFundsException extends Exception{
+	
+	private String message;
+	private double balance;
+	private double requestedAmount;
+	
+	public InsufficientFundsException(String message, double balance, double requestedAmount) {
+		super(message);
+		this.message = message;
+		this.balance = balance;
+		this.requestedAmount = requestedAmount;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public double getRequestedAmount() {
+		return requestedAmount;
+	}
+
+	public void setRequestedAmount(double requestedAmount) {
+		this.requestedAmount = requestedAmount;
+	}
+	
+}
