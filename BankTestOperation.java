26ec3cc80f3117ae8f08a0d97e3ff187a05461fe
Rembrant B. Tortogo <rbtortogo@headoffice.corp.bpi.com.ph>
2026-01-14 14:38:43 +0800

    M4 Activity 6

---
+++ BankTestOperation.java
@@ -0,0 +1,7 @@
+package m4_activity6;
+
+public interface BankTestOperation {
+	
+	void execute() throws InvalidAmountException, InsufficientFundsException;
+
+}
---
+++ InvalidAmountException.java
@@ -0,0 +1,8 @@
+package m4_activity6;
+
+public class InvalidAmountException extends Exception{
+
+	public InvalidAmountException(String string){
+		super(string);
+	}
+}
